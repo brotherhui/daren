d0bdb3672d4515af5265a8b704e09e5f1d147d3f
wuyb <wuyingbo@rongcapital.cn>
2018-06-11 17:49:36 +0800

    闪耀

---
+++ flare-shelf-api/src/main/java/org/sanpao/flare/shelf/api/App.java
@@ -0,0 +1,13 @@
+package org.sanpao.flare.shelf.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ flare-shelf-service/src/main/java/org/sanpao/flare/shelf/service/App.java
@@ -0,0 +1,13 @@
+package org.sanpao.flare.shelf.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
