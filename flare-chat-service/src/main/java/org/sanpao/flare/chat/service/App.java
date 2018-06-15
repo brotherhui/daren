2e5fc0bf7d90205039923e534551cc320ddc797f
wuyb <wuyingbo@rongcapital.cn>
2018-06-15 18:02:26 +0800

    闪耀

---
+++ flare-chat-service/src/main/java/org/sanpao/flare/chat/service/App.java
@@ -0,0 +1,13 @@
+package org.sanpao.flare.chat.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
