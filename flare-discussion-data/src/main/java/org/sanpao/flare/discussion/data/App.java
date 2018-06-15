272c5662a496596ddc7c4ad81784070d47cc5a8a
wuyb <wuyingbo@rongcapital.cn>
2018-06-15 16:13:21 +0800

    新增讨论微服务

---
+++ flare-discussion-data/src/main/java/org/sanpao/flare/discussion/data/App.java
@@ -0,0 +1,13 @@
+package org.sanpao.flare.discussion.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
